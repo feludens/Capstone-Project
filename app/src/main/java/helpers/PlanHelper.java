9f9dab80ce187013106e6471e92c2b5abc3cacc3
Felipe Pereira <fe.ludens@gmail.com>
2016-04-29 19:35:03 -0400

    Create PlanHelper method to add a Sample Plan to new accounts

--- app/src/main/java/helpers/PlanHelper.java
+++ app/src/main/java/helpers/PlanHelper.java
@@ -1,7 +1,24 @@
 package helpers;
 
+import com.spadatech.mobile.android.foodframer.R;
+
+import io.realm.Realm;
+import models.Plan;
+import models.User;
+
 /**
- * Created by pereirf on 4/29/16.
+ * Created by Felipe S. Pereira on 4/29/16.
  */
 public class PlanHelper {
+
+    public void addNewPlan(Realm realm, User user){
+        if(user.getPlanList().size() == 0){
+            realm.beginTransaction();
+            Plan samplePlan = realm.createObject(Plan.class);
+            samplePlan.setPlanName("Sample Plan");
+            samplePlan.setImageRsc(R.drawable.google);
+            samplePlan.setWeekdaysList(WeekdayHelper.newWeekdayList());
+            realm.commitTransaction();
+        }
+    }
 }
