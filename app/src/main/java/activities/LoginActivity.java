eea1b93f8507291265acb27ea98ebe2933910ea5
Felipe Pereira <fe.ludens@gmail.com>
2016-05-02 22:38:34 -0400

    Handled creating a new session after login

--- app/src/main/java/activities/LoginActivity.java
+++ app/src/main/java/activities/LoginActivity.java
@@ -1,9 +1,10 @@
 package activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -11,6 +12,7 @@ import com.spadatech.mobile.android.foodframer.R;
 
 import io.realm.Realm;
 import io.realm.RealmResults;
+import managers.SessionManager;
 import models.User;
 
 public class LoginActivity extends AppCompatActivity {
@@ -28,9 +30,6 @@ public class LoginActivity extends AppCompatActivity {
         EditText username = (EditText) findViewById(R.id.et_login_username);
         EditText password = (EditText) findViewById(R.id.et_login_password);
 
-        Log.d("Ludens", "username: " + username.getText().toString());
-        Log.d("Ludens", "password: " + password.getText().toString());
-
         Realm realm = Realm.getDefaultInstance();
         RealmResults<User> result = realm.where(User.class)
                 .equalTo("username", username.getText().toString())
@@ -38,8 +37,24 @@ public class LoginActivity extends AppCompatActivity {
                 .equalTo("email", username.getText().toString())
                 .findAll();
 
-        if(result.first().getPassword().equals(password.getText().toString())){
-            Log.d("Ludens", "SUCCESS");
+        if(result.size() > 0 && result.first().getPassword().equals(password.getText().toString())){
+            SessionManager sessionManager = new SessionManager(this);
+            if(sessionManager.createSession(result.first().getUsername(), result.first().getEmail())){
+                Intent intent = new Intent(this, PlanListActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        }else{
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setTitle("Ooopsie!");
+            alertDialog.setMessage("You enter the wrong credentials. Try again!");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Got it!",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
         }
 
     }
