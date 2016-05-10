1afb705cba2f0ed93c252553706e91798a1b7d37
Felipe Pereira <fe.ludens@gmail.com>
2016-05-10 19:55:20 -0400

    Updated WeekdayHelper to add weekday image upon creation of the object

--- app/src/main/java/com/spadatech/mobile/android/foodframer/helpers/WeekdayHelper.java
+++ app/src/main/java/com/spadatech/mobile/android/foodframer/helpers/WeekdayHelper.java
@@ -1,5 +1,7 @@
 package com.spadatech.mobile.android.foodframer.helpers;
 
+import android.content.Context;
+
 import com.spadatech.mobile.android.foodframer.models.Weekday;
 
 import java.util.Arrays;
@@ -15,19 +17,22 @@ public class WeekdayHelper {
 
     private static List<String> mWeekdayNameList = Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
 
-    public static RealmList<Weekday> newWeekdayList(Realm realm) {
+    public static RealmList<Weekday> newWeekdayList(Realm realm, Context context) {
         RealmList<Weekday> weekdayList = new RealmList<>();
 
         while (weekdayList.size() < 7){
-            weekdayList.add(createNewWeekday(realm, weekdayList.size()));
+            weekdayList.add(createNewWeekday(realm, weekdayList.size(), context));
         }
 
         return weekdayList;
     }
 
-    private static Weekday createNewWeekday(Realm realm, int size) {
+    private static Weekday createNewWeekday(Realm realm, int size, Context context) {
         Weekday newWeekday = realm.createObject(Weekday.class);
         newWeekday.setWeekdayName(mWeekdayNameList.get(size));
+        int resourceId = context.getResources().getIdentifier(mWeekdayNameList.get(size).toLowerCase(), "drawable", context.getPackageName());
+        newWeekday.setImage(resourceId);
+
         return newWeekday;
 //        newWeekday.setGroceryList();
 //        newWeekday.setMealList();
