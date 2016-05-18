4cad7a61936479a137e825b589265e33090daa83
Felipe Pereira <fe.ludens@gmail.com>
2016-05-17 21:19:30 -0400

    Created cardview_prepday_item, and updated PrepdayItemListAdapter

--- app/src/main/java/com/spadatech/mobile/android/foodframer/adapters/PrepdayItemListAdapter.java
+++ app/src/main/java/com/spadatech/mobile/android/foodframer/adapters/PrepdayItemListAdapter.java
@@ -1,7 +1,6 @@
 package com.spadatech.mobile.android.foodframer.adapters;
 
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,13 +27,15 @@ public class PrepdayItemListAdapter extends RecyclerView.Adapter<PrepdayItemList
         }
     }
 
-    public class MealViewHolder extends ViewHolder {
+    public class PrepdayViewHolder extends ViewHolder {
         TextView name;
+        TextView note;
         ImageButton deleteButton;
 
-        public MealViewHolder(View v) {
+        public PrepdayViewHolder(View v) {
             super(v);
             this.name = (TextView) v.findViewById(R.id.tv_item_name);
+            this.name = (TextView) v.findViewById(R.id.tv_item_note);
             this.deleteButton = (ImageButton) v.findViewById(R.id.ib_delete);
         }
     }
@@ -47,19 +48,20 @@ public class PrepdayItemListAdapter extends RecyclerView.Adapter<PrepdayItemList
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         View v = LayoutInflater.from(viewGroup.getContext())
-                .inflate(R.layout.listview_meal_item, viewGroup, false);
+                .inflate(R.layout.listview_prepday_item, viewGroup, false);
 
-        return new MealViewHolder(v);
+        return new PrepdayViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, final int position) {
-        Log.d("Ludens", "mList.get(position): " + mList.get(position));
         MealItem mealItem = (MealItem) mList.get(position);
         String name = mealItem.getMealItemName();
+        String note = mealItem.getMealItemNotes();
 
-        MealViewHolder holder = (MealViewHolder) viewHolder;
+        PrepdayViewHolder holder = (PrepdayViewHolder) viewHolder;
         holder.name.setText(name);
+        holder.note.setText(note);
 
         if(mIsEditMode){
             holder.deleteButton.setVisibility(View.VISIBLE);
