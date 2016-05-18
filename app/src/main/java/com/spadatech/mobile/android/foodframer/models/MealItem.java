2d3625eed56cb818d8c8a9fa6d94f87831291b96
Felipe Pereira <fe.ludens@gmail.com>
2016-05-17 20:34:59 -0400

    Updated MealItem model class

--- app/src/main/java/com/spadatech/mobile/android/foodframer/models/MealItem.java
+++ app/src/main/java/com/spadatech/mobile/android/foodframer/models/MealItem.java
@@ -1,36 +1,35 @@
 package com.spadatech.mobile.android.foodframer.models;
 
-import io.realm.RealmList;
 import io.realm.RealmObject;
 
 /**
  * Created by Felipe S. Pereira on 4/13/16.
  */
 public class MealItem extends RealmObject {
-    private String mMealItemName;
-    private RealmList<MealInstructions> mMealInstructions;
+    private String name;
+    private String notes;
 
     public MealItem() {
     }
 
-    public MealItem(String mMealItemName, RealmList<MealInstructions> mMealInstructions) {
-        this.mMealItemName = mMealItemName;
-        this.mMealInstructions = mMealInstructions;
+    public MealItem(String name, String notes) {
+        this.name = name;
+        this.notes = notes;
     }
 
     public String getMealItemName() {
-        return mMealItemName;
+        return name;
     }
 
     public void setMealItemName(String mMealItemName) {
-        this.mMealItemName = mMealItemName;
+        this.name = mMealItemName;
     }
 
-    public RealmList<MealInstructions> getmMealInstructions() {
-        return mMealInstructions;
+    public String getMealItemNotes() {
+        return notes;
     }
 
-    public void setMealInstructions(RealmList<MealInstructions> mMealInstructions) {
-        this.mMealInstructions = mMealInstructions;
+    public void setMealItemNotes(String notes) {
+        this.notes = notes;
     }
 }
