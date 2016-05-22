bde2a526cc59afc547d399da76f04bc7a626a08f
Felipe Pereira <fe.ludens@gmail.com>
2016-05-21 21:29:04 -0400

    Created DBHelper

---
+++ app/src/main/java/com/spadatech/mobile/android/foodframer/helpers/DBHelper.java
@@ -0,0 +1,29 @@
+package com.spadatech.mobile.android.foodframer.helpers;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Felipe S. Pereira on 5/21/16.
+ */
+public class DBHelper extends SQLiteOpenHelper{
+
+    // DB Version
+    private static final int DATABASE_VERSION = 1;
+    // DB Name
+    private static final String DATABASE_NAME = "sqliteFoodFramer.db";
+
+    public DBHelper( ) {
+        super(App.getContext(), DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
